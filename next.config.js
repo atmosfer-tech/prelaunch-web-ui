62d7a381baa4f671d61f24cafd657baa2923675a
İlker Korkut <ilker@arf.one>
2022-10-30 04:11:23 +0300

    initialize

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'prod'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/prelaunch-web-ui/' : '',
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
